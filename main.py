298ca07813df9b9ecbe59fced89afdaa801fc885
Aarav Singh <53440217+Aarav6790@users.noreply.github.com>
2024-11-23 16:07:24 +0530

    3

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import nltk
-tokenizer = nltk.tokenize.RegexpTokenizer('[^\w\']+', gaps=True)
-text = 'My name is Aarav and I am a student of class 10th. I am learning Python.'
-tokens = tokenizer.tokenize(text)
-tokens = [w for w in tokens if not w in nltk.corpus.stopwords.words('english')]
-print(tokens)
+
+def remove_stopwords(sentence):
+    tokenizer = nltk.tokenize.RegexpTokenizer('[^\w\']+', gaps=True)
+    text = 'My name is Aarav and I am a student of class 10th. I am learning Python.'
+    tokens = tokenizer.tokenize(text)
+    tokens = [w for w in tokens if not w in nltk.corpus.stopwords.words('english')]
+    return tokens
